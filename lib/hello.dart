2e3936910f8df7edccfec3ebc9d58d3d0562b183
Hotaka Makiuchi <makiuchi@ccube.jp>
2022-10-16 07:10:58 +0900

    change title

--- lib/hello.dart
+++ lib/hello.dart
@@ -12,10 +12,10 @@ class HelloApp extends StatelessWidget {
   Widget build(BuildContext context) {
     final word = WordPair.random();
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+          title: const Text('Startup Name Generator'),
         ),
         body: const Center(
           child: RandomWords(),
