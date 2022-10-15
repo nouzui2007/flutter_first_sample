120ba81c45142a9eae29703f80ba891949fed3ec
Hotaka Makiuchi <makiuchi@ccube.jp>
2022-10-16 06:57:18 +0900

    add stateful widget

--- lib/hello.dart
+++ lib/hello.dart
@@ -17,10 +17,28 @@ class HelloApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: Center(
-          child: Text(word.asPascalCase),
+        body: const Center(
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggestFont = const TextStyle(fontSize: 18);
+  
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
